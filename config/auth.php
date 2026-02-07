5deb773cf8148de2add511fd845774a1db9c9fe1
dev.dabour <dabourdabour28@gmail.com>
2026-02-07 16:49:36 +0200

    feat(auth): implement JWT authentication and update user model

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => 'api',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -36,6 +36,12 @@ return [
     */
 
     'guards' => [
+
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
